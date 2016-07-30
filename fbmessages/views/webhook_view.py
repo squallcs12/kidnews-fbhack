ce75ce237cdeb58e8715b28f667d1aee86fed222
Bang Dao <daotranbang@gmail.com>
2016-07-31 05:39:32 +0700

    fix webhook login on receive text login

--- fbmessages/views/webhook_view.py
+++ fbmessages/views/webhook_view.py
@@ -129,7 +129,7 @@ class WebhookView(APIView):
         sender_id = self.get_sender_id(message)
         message_text = message['message']['text']
         if message_text == 'login':
-            message_service.send_login_button(sender_id)
+            self.send_login_button(sender_id)
         elif message_text == 'notification':
             message_service.send_notification_settings_message(sender_id)
         else:
